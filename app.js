ad351ffca32f8a41579d09579d1e2e35d5fbcd22
KesterDaniel <kesterdan17@gmail.com>
2022-09-28 09:59:47 +0100

    created merchant model

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require("express")
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+const methodOverride = require("method-override")
+const passport = require("passport")
+// const expressSession = require("express-session")
+const passportLocalMongose = require("passport-local-mongoose")
+const LocalStrategy = require("passport-local")
+const productRoute = require("./routes/products")
+const userRoute = require("./routes/users")
+const indexRoute = require("./routes/index")
 const app = express()
 const port = process.env.PORT
 
-app.get("/", (req, res)=>{
-    res.send("Welcome to TradeConnect")
-})
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(methodOverride("_method"))
+app.set("view engine", "ejs")
+
+// app.use(productRoute)
+// app.use(userRoute)
+app.use(indexRoute)
 
 app.listen(port, ()=>{
     console.log("server up")
---
+++ models/merchant.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose")
+const PLM = require("passport-local-mongoose")
+
+const MerchantSchema = new mongoose.Schema({
+    Name: {
+        type: String,
+        required: true
+    },
+    State: {
+        type: String,
+        required: true
+    },
+    Address: {
+        type: String,
+        required: true
+    },
+    PhoneNumber: {
+        type: String,
+        required: true
+    },
+    DisplayPic: {
+        type: String,
+        required: true
+    },
+    Messages: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Message"
+    }], 
+    Orders: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Order"
+    }]
+})
+
+MerchantSchema.plugin(passportLocalMongoose)
+const Merchant = mongoose.model("Merchant", MerchantSchema)
+
+module.exports = Merchant
