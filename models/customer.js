fa269198ee1aa39ca5de72f4c49b72f1132b7b86
KesterDaniel <kesterdan17@gmail.com>
2022-09-28 12:07:19 +0100

    created customer model

---
+++ models/customer.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+const passportLocalMongoose = require("passport-local-mongoose")
+
+const CustomerSchema = new mongoose.Schema({
+    Name:{
+        type: String,
+        required: true
+    },
+    PhoneNumber:{
+        type: String,
+        required: true
+    },
+    Address:{
+        type: String,
+        required: true
+    },
+    State:{
+        type: String,
+        required: true
+    },
+    Orders:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Order"
+    }]
+})
+
+CustomerSchema.plugin(passportLocalMongoose)
+const Customer = mongoose.model("Customer", CustomerSchema)
--- models/product.js
+++ models/product.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose")
-const passportLocalMongoose = require("passport-local-mongoose")
 
 const ProductSchema = new mongoose.Schema({
     ProductName: {
